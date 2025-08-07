31a89f9130bc2c6e3a4207d274e4bfc36ca5fcc1
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-08-08 01:13:03 +0200

    modification des plants

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -7,7 +7,7 @@ function handleClick(plantName) {
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
