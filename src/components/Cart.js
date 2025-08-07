3db58bab2e67aff3c0c770f78b1a5ca910190864
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-08-08 01:26:30 +0200

    modification du component  de la cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,6 @@ import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-    const items = Object.keys(cart)
 	const total = cart.reduce(
 		(acc, plantType) => acc + plantType.amount * plantType.price,
 		0
