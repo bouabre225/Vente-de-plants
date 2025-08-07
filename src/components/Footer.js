e3df2e0b3c9284bf2303e28a137edbbd3cd975b4
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-08-08 01:04:15 +0200

    ajout du component de footer

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+	const [inputValue, setInputValue] = useState('')
+
+	function handleInput (e) {
+		setInputValue(e.target.value)
+	}
+
+	function handlrBlur () {
+		if (!inputValue.includes('@')) alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+	}
+
+	return ( 
+		<footer className='lmj-footer'>
+			<div className='lmj-footer-elem'>
+				Pour les passionné·e·s de plantes 🌿🌱🌵
+			</div>
+			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				placeholder='Laissez votre e-mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handlrBlur}
+			/>
+		</footer>
+	)
+}
+
+export default Footer
